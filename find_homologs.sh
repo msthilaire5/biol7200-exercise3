d88bc52cc0a3f64fc75fd502ce82825e3c0d041d
Megan St. Hilaire <mfsthilaire13@gmail.com>
2025-09-07 12:46:10 -0400

    Redirect BLAST output to output_file

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,4 +4,4 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
-tblastn -query $query_file -subject $subject_file -outfmt '6 std qlen'
+tblastn -query $query_file -subject $subject_file -outfmt '6 std qlen' > $output_file
