bc27a2199cd98e56fc016115ad4f7dfe2c6d7559
Megan St. Hilaire <mfsthilaire13@gmail.com>
2025-09-07 12:41:47 -0400

    Include query sequence length in BLAST output

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,4 +4,4 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
-tblastn -query $query_file -subject $output_file -outfmt '6 std'
+tblastn -query $query_file -subject $output_file -outfmt '6 std qlen'
