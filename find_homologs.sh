50019f05285910e2d52b4545d100c5edc697f6dc
Megan St. Hilaire <mfsthilaire13@gmail.com>
2025-09-07 22:25:04 -0400

    Emphasize line count in output

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,9 +4,7 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
-tblastn -query $query_file -subject $subject_file -outfmt '6 std qlen' | \
-    awk '($3 > 30) && ($4 > 0.9 * $13)' \
-    > $output_file
+tblastn -query $query_file -subject $subject_file -outfmt '6 std qlen' > $output_file
 
 echo 'Number of hits:'
 wc -l $output_file
