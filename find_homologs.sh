2920b18b68f09c571bf62f28b6a5f4a1f6012a44
Megan St. Hilaire <mfsthilaire13@gmail.com>
2025-09-08 19:09:08 -0400

    Revert "Emphasize line count in output"
    This reverts commit 50019f05285910e2d52b4545d100c5edc697f6dc.

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,7 +4,9 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
-tblastn -query $query_file -subject $subject_file -outfmt '6 std qlen' > $output_file
+tblastn -query $query_file -subject $subject_file -outfmt '6 std qlen' | \
+    awk '($3 > 30) && ($4 > 0.9 * $13)' \
+    > $output_file
 
 echo 'Number of hits:'
 wc -l $output_file
